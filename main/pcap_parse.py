05269ea04a5d9c347a84d15c0ab4750577a5763c
8ethan <weitkampe@etown.edu>
2023-04-09 22:12:29 -0400

    Fixed a lot of issues with pcap parsing and generating dataframes from raw data

--- main/pcap_parse.py
+++ main/pcap_parse.py
@@ -28,7 +28,13 @@ def parse_v1(f, df, label):
 
     f.seek(-4, 1)
 
-    test = []
+    all_bytes = []
+    timestamps = []
+    src_ips = []
+    src_ports = []
+    dst_ips = []
+    dst_ports = []
+    protos = [1,6,17] # Acceptable protocols (ICMP, TCP, UDP)
     last_loc = 0
     # Loop through all packets
     while f.tell() != last_loc:
@@ -39,20 +45,37 @@ def parse_v1(f, df, label):
         if p_block_length==0:
             break
         interfaceID = f.read(4)
-        timestamp1 = f.read(4)
-        timestamp2 = f.read(4)
-        if p_block_length < 24:
+        ts1 = f.read(4)
+        ts2 = f.read(4)
+        ts_actual = (int.from_bytes(ts1, "little") << 32)|(int.from_bytes(ts2, "little"))
+        cap_len = f.read(4)
+        p_len = f.read(4)
+        if p_block_length < 32:
             print("Block length too short?")
             print(p_block_length)
             print(len(test))
             break
-        data = f.read(p_block_length-24)
+        data = f.read(p_block_length-32)
         f.read(4)
-
-        test.append(data)
         
-    temp = pd.DataFrame(data=test, columns=["p_bytes"])
+        # Only include packet if its from outside source, ipv4, and TCP/UDP/ICMP protocol
+        #if data[1] != 4 and data[16]!=96 and data[25] in protos:
+        timestamps.append(round(ts_actual/1000000))
+        src_ips.append( str(data[28])+"."+str(data[29])+"."+str(data[30])+"."+str(data[31]) )
+        dst_ips.append( str(data[32])+"."+str(data[33])+"."+str(data[34])+"."+str(data[35]) )
+        src_ports.append(int.from_bytes(data[36:38],"big"))
+        dst_ports.append(int.from_bytes(data[38:40],"big"))
+        all_bytes.append(data)
+            
+        
+    temp = pd.DataFrame(data=all_bytes, columns=["p_bytes"])
     temp["label"] = label
+    temp["pcap_ver"] = 1
+    temp["ts"] = timestamps
+    temp["src_ip"] = src_ips
+    temp["dst_ip"] = dst_ips
+    temp["src_port"] = src_ports
+    temp["dst_port"] = dst_ports
     new_df = pd.concat([df, temp], axis=0)
     new_df.reset_index(drop=True, inplace=True)
     return new_df
@@ -66,7 +89,13 @@ def parse_v2(f, df, label):
     snap_len = f.read(4)
     link_type = f.read(4)
     
-    test = []
+    all_bytes = []
+    timestamps = []
+    src_ips = []
+    src_ports = []
+    dst_ips = []
+    dst_ports = []
+    protos = [1,6,17] # Acceptable protocols (ICMP, TCP, UDP)
     last_loc = 0
     while f.tell() != last_loc:
         #Read in packet header
@@ -74,14 +103,29 @@ def parse_v2(f, df, label):
         ts1 = f.read(4)
         ts2 = f.read(4)
         cap_len = f.read(4)
-        if int.from_bytes(cap_len, "little") == 0:
-            break
+        #if int.from_bytes(cap_len, "little") == 0:
+        #    break
         p_len = f.read(4)
         data = f.read(int.from_bytes(cap_len, "little"))
-        test.append(data)
         
-    temp = pd.DataFrame(data=test, columns=["p_bytes"])
+        # Only include packet if its from outside source, ipv4, and TCP/UDP/ICMP protocol
+        #if data[1] != 4 and data[16]!=96 and data[25] in protos:
+        if len(data) > 0:
+            timestamps.append( round( int.from_bytes(ts1, "little")+(int.from_bytes(ts2, "little")/1000000) ) )
+            src_ips.append( str(data[28])+"."+str(data[29])+"."+str(data[30])+"."+str(data[31]) )
+            dst_ips.append( str(data[32])+"."+str(data[33])+"."+str(data[34])+"."+str(data[35]) )
+            src_ports.append(int.from_bytes(data[36:38], "big"))
+            dst_ports.append(int.from_bytes(data[38:40], "big"))
+            all_bytes.append(data)
+        
+    temp = pd.DataFrame(data=all_bytes, columns=["p_bytes"])
     temp["label"] = label
+    temp["ts"] = timestamps
+    temp["src_ip"] = src_ips
+    temp["dst_ip"] = dst_ips
+    temp["src_port"] = src_ports
+    temp["dst_port"] = dst_ports
+    temp["pcap_ver"] = 2
     new_df = pd.concat([df, temp], axis=0)
     new_df.reset_index(drop=True, inplace=True)
     return new_df
